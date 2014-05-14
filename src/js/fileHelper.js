2e556b5c12da0474150785249dbae59fdf9fbbd5
javorosas <javo@wavestack.com>
2014-05-14 14:21:22 -0700

    Create the Wavestack folder in the user's directory

---
+++ src/js/fileHelper.js
@@ -0,0 +1,13 @@
+FileHelper = function () {
+};
+
+FileHelper.prototype.createWavestackFolder = function () {
+	var fs = require('fs');
+
+	console.log("Creating Wavestack folder...")
+	fs.mkdir((process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + "/Wavestack", function (err) {
+		if (err.code == "EEXIST") console.log("Folder already exists.");
+	});
+};
+
+fileHelper = new FileHelper();
