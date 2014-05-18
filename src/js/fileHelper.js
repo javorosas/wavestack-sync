685f006f3244696ef2039f68fb0d157ef6345193
javorosas <javo@wavestack.com>
2014-05-17 23:08:23 -0700

    add getLocalFiles method

--- src/js/fileHelper.js
+++ src/js/fileHelper.js
@@ -8,3 +8,31 @@ fileHelper.createWavestackFolder = function () {
 		if (err.code == "EEXIST") console.log("Folder already exists.");
 	});
 };
+
+/*
+	callback = function (err, cloudFiles)
+	A CloudFile JSON object has the following structure:
+	{
+		RelativePath: "path/to/file.txt",
+		ModifiedDate: "1990-01-01T14:55:58.201Z"
+	}
+*/
+fileHelper.getLocalFiles = function(callback) {
+	var recursive = require('recursive-readdir');
+	recursive(this.wavestackFolder, function (err, files) {
+		if (!err) {
+			var cloudFiles = [];
+			var fs = require('fs');
+			for (var f in files) {
+				var stat = f.statSync(f);
+				if (stat.isFile()) {
+					var relative = f.slice(this.wavestackFolder.length);
+					cloudFiles.push({
+						RelativePath: relative,
+						ModifiedDate: stat.mtime.toISOString()
+					});
+				}
+			}
+		}
+	});
+};
