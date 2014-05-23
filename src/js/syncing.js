07d9a20cbf8e4dbb3093d9ed589cd0069c761391
javorosas <javo@wavestack.com>
2014-05-22 19:56:58 -0700

    fix change reference status to statusCode

--- src/js/syncing.js
+++ src/js/syncing.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
 	syncHelper.startSyncing(function (err, status, percentage, task) {
 		if (err) {
 
-		} else if (status == syncHelper.status.completed) {
+		} else if (status == syncHelper.statusCode.completed) {
 			$('#main .log').append('<div>Sync completed</div>');
 			$('#main .progress-bar').css('width', '100%');
 			$('#main .progress').removeClass('active');
 			$('#main h3').text('Synced');
-		} else if (status = syncHelper.status.running) {
+		} else if (status = syncHelper.statusCode.running) {
 			$('#main h3').text('Syncing...');
 			$('#main .progress-bar').css('width', (percentage * 100) + '%');
 			var $log = $('#main .log');
